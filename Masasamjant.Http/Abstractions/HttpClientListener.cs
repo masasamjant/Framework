3f3d8a29bdbd520691089f63544058631b59cef7
Matti Jantti <m_jantti@hotmail.com>
2024-11-08 19:35:24 +0200

    Add abstract HTTP listeners

---
+++ Masasamjant.Http/Abstractions/HttpClientListener.cs
@@ -0,0 +1,29 @@
+﻿namespace Masasamjant.Http.Abstractions
+{
+    /// <summary>
+    /// Represents abstract listener of HTTP request execution.
+    /// </summary>
+    public abstract class HttpClientListener : IHttpClientListener
+    {
+        /// <summary>
+        /// Invoked before specified HTTP request is executed.
+        /// </summary>
+        /// <param name="request">The HTTP request.</param>
+        /// <returns>A <see cref="Task"/>.</returns>
+        public abstract Task OnErrorAsync(HttpRequest request, Exception exception);
+
+        /// <summary>
+        /// Invoked after specified HTTP request is executed.
+        /// </summary>
+        /// <param name="request">The HTTP request.</param>
+        /// <returns>A <see cref="Task"/>.</returns>
+        public abstract Task OnExecutedAsync(HttpRequest request);
+
+        /// <summary>
+        /// Invoked before specified HTTP request is executed.
+        /// </summary>
+        /// <param name="request">The HTTP request.</param>
+        /// <returns>A <see cref="Task"/>.</returns>
+        public abstract Task OnExecutingAsync(HttpRequest request);
+    }
+}
---
+++ Masasamjant.Http/Abstractions/IHttpClientListener.cs
@@ -0,0 +1,30 @@
+﻿namespace Masasamjant.Http.Abstractions
+{
+    /// <summary>
+    /// Represents listener of HTTP request execution.
+    /// </summary>
+    public interface IHttpClientListener
+    {
+        /// <summary>
+        /// Invoked before specified HTTP request is executed.
+        /// </summary>
+        /// <param name="request">The HTTP request.</param>
+        /// <returns>A <see cref="Task"/>.</returns>
+        Task OnExecutingAsync(HttpRequest request);
+
+        /// <summary>
+        /// Invoked after specified HTTP request is executed.
+        /// </summary>
+        /// <param name="request">The HTTP request.</param>
+        /// <returns>A <see cref="Task"/>.</returns>
+        Task OnExecutedAsync(HttpRequest request);
+
+        /// <summary>
+        /// Invoked if exception occurs when executing specified HTTP request.
+        /// </summary>
+        /// <param name="request">The HTTP request.</param>
+        /// <param name="exception">The occurred exception.</param>
+        /// <returns>A <see cref="Task"/>.</returns>
+        Task OnErrorAsync(HttpRequest request, Exception exception);
+    }
+}
