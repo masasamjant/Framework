f8c0004da4706449b727cf8fdc6cac4e9183e677
Matti Jantti <m_jantti@hotmail.com>
2024-11-11 10:42:52 +0200

    Interface to indicate that object can be serialized to JSON

---
+++ Masasamjant.Core/IJsonSerializable.cs
@@ -0,0 +1,9 @@
+﻿namespace Masasamjant
+{
+    /// <summary>
+    /// Represents object that can be serialized to JSON string.
+    /// </summary>
+    public interface IJsonSerializable
+    {
+    }
+}
