92b9ffdf70f4bfc3d34c4cef5ae0e761b1f7996d
Matti Jantti <m_jantti@hotmail.com>
2024-11-11 11:22:53 +0200

    Add repositories

---
+++ Masasamjant.Repositories/IConnectionStringProvider.cs
@@ -0,0 +1,14 @@
+﻿namespace Masasamjant.Repositories
+{
+    /// <summary>
+    /// Represents provider of connection string.
+    /// </summary>
+    public interface IConnectionStringProvider
+    {
+        /// <summary>
+        /// Gets the connection string.
+        /// </summary>
+        /// <returns>A connection string.</returns>
+        string GetConnectionString();
+    }
+}
---
+++ Masasamjant.Repositories/IRepository.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Masasamjant.Repositories
+{
+    /// <summary>
+    /// Represents repository to save data of objects.
+    /// </summary>
+    public interface IRepository : IDisposable
+    {
+        Task<T> AddAsync<T>(T instance, bool saveChanges = false) where T : class;
+
+        Task<bool> ExistsAsync<T>(Expression<Func<T, bool>> criteria) where T : class;
+
+        Task<IReadOnlyCollection<T>> GetAsync<T>() where T : class;
+
+        Task<IReadOnlyCollection<T>> GetAsync<T>(Expression<Func<T, bool>> criteria) where T : class;
+
+        IQueryable<T> Query<T>() where T : class;
+
+        IQueryable<T> Query<T>(Expression<Func<T, bool>> criteria) where T : class;
+
+        Task<T> RemoveAsync<T>(T instance, bool saveChanges = false) where T : class;
+
+        Task<int> SaveChangesAsync();
+
+        Task<T> UpdateAsync<T>(T instance, bool saveChanges = false) where T : class;
+    }
+}
