61ef0ed147856269a3c30f3fc1effce6110a49b6
Matti Jantti <m_jantti@hotmail.com>
2024-11-12 15:13:21 +0200

    Add repository abstractions and base implementation using entity framework

--- Masasamjant.Repositories/Abstractions/IRepository.cs
+++ Masasamjant.Repositories/Abstractions/IRepository.cs
@@ -1,6 +1,4 @@
-﻿using System.Linq.Expressions;
-
-namespace Masasamjant.Repositories.Abstractions
+﻿namespace Masasamjant.Repositories.Abstractions
 {
     /// <summary>
     /// Represents repository to save data of objects.
@@ -13,36 +11,45 @@ namespace Masasamjant.Repositories.Abstractions
         /// <typeparam name="T">The type of the object.</typeparam>
         /// <param name="instance">The <typeparamref name="T"/> instance to add.</param>
         /// <param name="saveChanges"><c>true</c> to save changes immediately; <c>false</c> if <see cref="SaveChangesAsync"/> is invoked afterwards.</param>
+        /// <param name="cancellationToken">The cancellation token.</param>
         /// <returns>A added <typeparamref name="T"/> instance.</returns>
         /// <exception cref="RepositoryException">If exception occurs at operation.</exception>
-        Task<T> AddAsync<T>(T instance, bool saveChanges = false) where T : class;
+        /// <exception cref="ConcurrentUpdateException">If concurrent update error occurs.</exception>
+        /// <exception cref="OperationCanceledException">If operation is cancelled.</exception>
+        Task<T> AddAsync<T>(T instance, bool saveChanges = false, CancellationToken cancellationToken = default) where T : class;
 
         /// <summary>
         /// Check if repository contains any instance of <typeparamref name="T"/> that match specified criteria.
         /// </summary>
         /// <typeparam name="T">The type of the object.</typeparam>
-        /// <param name="criteria">The criteria expression to match.</param>
-        /// <returns><c>true</c> if repository contains any instance of <typeparamref name="T"/> that match <paramref name="criteria"/>; <c>false</c> otherwise.</returns>
+        /// <param name="specification">The criteria specification to match.</param>
+        /// <param name="cancellationToken">The cancellation token.</param>
+        /// <returns><c>true</c> if repository contains any instance of <typeparamref name="T"/> that match criteria of <paramref name="specification"/>; <c>false</c> otherwise.</returns>
         /// <exception cref="RepositoryException">If exception occurs at operation.</exception>
-        Task<bool> ExistsAsync<T>(Expression<Func<T, bool>> criteria) where T : class;
+        /// <exception cref="OperationCanceledException">If operation is cancelled.</exception>
+        Task<bool> ExistsAsync<T>(IQuerySpecification<T> specification, CancellationToken cancellationToken = default) where T : class;
 
         /// <summary>
         /// Gets all instance of <typeparamref name="T"/> in repository. This method can be used in case the amount of instance of <typeparamref name="T"/> is limited 
         /// and it is known that all those are needed.
         /// </summary>
         /// <typeparam name="T">The type of the object.</typeparam>
-        /// <returns>A collection of <typeparamref name="T"/> instances in repository.</returns>
+        /// <param name="cancellationToken">The cancellation token.</param>
+        /// <returns>A list of <typeparamref name="T"/> instances in repository.</returns>
         /// <exception cref="RepositoryException">If exception occurs at operation.</exception>
-        Task<IReadOnlyCollection<T>> GetAsync<T>() where T : class;
+        /// <exception cref="OperationCanceledException">If operation is cancelled.</exception>
+        Task<List<T>> GetAsync<T>(CancellationToken cancellationToken = default) where T : class;
 
         /// <summary>
         /// Gets all instance of <typeparamref name="T"/> in repository that match specified criteria.
         /// </summary>
         /// <typeparam name="T">The type of the object.</typeparam>
-        /// <param name="criteria">The criteria expression to match.</param>
-        /// <returns>A collection of <typeparamref name="T"/> instances in repository that match <paramref name="criteria"/>.</returns>
+        /// <param name="specification">The criteria specification to match.</param>
+        /// <param name="cancellationToken">The cancellation token.</param>
+        /// <returns>A list of <typeparamref name="T"/> instances in repository that match criteria of <paramref name="specification"/>.</returns>
         /// <exception cref="RepositoryException">If exception occurs at operation.</exception>
-        Task<IReadOnlyCollection<T>> GetAsync<T>(Expression<Func<T, bool>> criteria) where T : class;
+        /// <exception cref="OperationCanceledException">If operation is cancelled.</exception>
+        Task<List<T>> GetAsync<T>(IQuerySpecification<T> specification, CancellationToken cancellationToken = default) where T : class;
 
         /// <summary>
         /// Query instances of <typeparamref name="T"/> in repository.
@@ -56,10 +63,10 @@ namespace Masasamjant.Repositories.Abstractions
         /// Query instances of <typeparamref name="T"/> in repository that match specified criteria.
         /// </summary>
         /// <typeparam name="T">The type of the object.</typeparam>
-        /// <param name="criteria">The criteria expression to match.</param>
-        /// <returns>A query of <typeparamref name="T"/> instance in repository that match <paramref name="criteria"/>.</returns>
+        /// <param name="specification">The criteria specification to match.</param>
+        /// <returns>A query of <typeparamref name="T"/> instance in repository that match criteria of <paramref name="specification"/>.</returns>
         /// <exception cref="RepositoryException">If exception occurs at operation.</exception>
-        IQueryable<T> Query<T>(Expression<Func<T, bool>> criteria) where T : class;
+        IQueryable<T> Query<T>(IQuerySpecification<T> specification) where T : class;
 
         /// <summary>
         /// Remove, delete permanently, instance of <typeparamref name="T"/> from repository.
@@ -67,18 +74,22 @@ namespace Masasamjant.Repositories.Abstractions
         /// <typeparam name="T">The type of the object.</typeparam>
         /// <param name="instance">The instance to remove.</param>
         /// <param name="saveChanges"><c>true</c> to save changes immediately; <c>false</c> if <see cref="SaveChangesAsync"/> is invoked afterwards.</param>
+        /// <param name="cancellationToken">The cancellation token.</param>
         /// <returns>A removed instance.</returns>
         /// <exception cref="RepositoryException">If exception occurs at operation.</exception>
         /// <exception cref="ConcurrentUpdateException">If concurrent update error occurs.</exception>
-        Task<T> RemoveAsync<T>(T instance, bool saveChanges = false) where T : class;
+        /// <exception cref="OperationCanceledException">If operation is cancelled.</exception>
+        Task<T> RemoveAsync<T>(T instance, bool saveChanges = false, CancellationToken cancellationToken = default) where T : class;
 
         /// <summary>
         /// Save changes made to instance stored in repository.
         /// </summary>
+        /// <param name="cancellationToken">The cancellation token.</param>
         /// <returns>A count of changed instances.</returns>
         /// <exception cref="RepositoryException">If exception occurs at operation.</exception>
         /// <exception cref="ConcurrentUpdateException">If concurrent update error occurs.</exception>
-        Task<int> SaveChangesAsync();
+        /// <exception cref="OperationCanceledException">If operation is cancelled.</exception>
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
 
         /// <summary>
         /// Update specified instance of <typeparamref name="T"/> in repository.
@@ -86,9 +97,11 @@ namespace Masasamjant.Repositories.Abstractions
         /// <typeparam name="T">The type of the object.</typeparam>
         /// <param name="instance">The instance to update.</param>
         /// <param name="saveChanges"><c>true</c> to save changes immediately; <c>false</c> if <see cref="SaveChangesAsync"/> is invoked afterwards.</param>
-        /// <returns>A update instance.</returns>
+        /// <param name="cancellationToken">The cancellation token.</param>
+        /// <returns>A updated instance.</returns>
         /// <exception cref="RepositoryException">If exception occurs at operation.</exception>
         /// <exception cref="ConcurrentUpdateException">If concurrent update error occurs.</exception>
-        Task<T> UpdateAsync<T>(T instance, bool saveChanges = false) where T : class;
+        /// <exception cref="OperationCanceledException">If operation is cancelled.</exception>
+        Task<T> UpdateAsync<T>(T instance, bool saveChanges = false, CancellationToken cancellationToken = default) where T : class;
     }
 }
---
+++ Masasamjant.Repositories/EntityFramework/DbContextHelper.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace Masasamjant.Repositories.EntityFramework
+{
+    /// <summary>
+    /// Provides helper methods to <see cref="DbContext"/> class.
+    /// </summary>
+    public static class DbContextHelper
+    {
+        /// <summary>
+        /// Gets the query string of spcified criteria expression.
+        /// </summary>
+        /// <typeparam name="TEntity">The type of the entity.</typeparam>
+        /// <param name="context">The <see cref="DbContext"/>.</param>
+        /// <param name="expression">The criteria expression.</param>
+        /// <returns>A query string or <c>null</c>.</returns>
+        public static string? GetQueryString<TEntity>(this DbContext context, Expression<Func<TEntity, bool>> expression) where TEntity : class
+        {
+            try
+            {
+                var query = context.Set<TEntity>().Where(expression);
+                var queryText = query.ToQueryString();
+                return queryText;
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+    }
+}
