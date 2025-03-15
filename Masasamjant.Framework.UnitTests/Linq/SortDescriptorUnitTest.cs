6b1f3f597c7ae8d14cab5178f33dabefd7618588
Matti Jantti <m_jantti@hotmail.com>
2025-03-15 15:51:42 +0200

    Add sort descriptor

---
+++ Masasamjant.Framework.UnitTests/Linq/SortDescriptorUnitTest.cs
@@ -0,0 +1,68 @@
+﻿namespace Masasamjant.Linq
+{
+    [TestClass]
+    public class SortDescriptorUnitTest : UnitTest
+    {
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void Test_Constructor_When_Sort_Order_Undefined()
+        {
+            new SortDescriptor("Foo", (SortOrder)999);
+        }
+
+        [TestMethod]
+        public void Test_Constructor()
+        {
+            var descriptor = new SortDescriptor("Foo", SortOrder.Descending);
+            Assert.AreEqual("Foo", descriptor.PropertyName);
+            Assert.AreEqual(SortOrder.Descending, descriptor.SortOrder);
+        }
+
+        [TestMethod]
+        public void Test_Not_Equal()
+        {
+            var descriptor = new SortDescriptor("Foo", SortOrder.Descending);
+            var other = new SortDescriptor("Bar", SortOrder.Descending);
+            Assert.IsFalse(descriptor.Equals(other));
+            other = new SortDescriptor("Foo", SortOrder.Ascending);
+            Assert.IsFalse(descriptor.Equals(other));
+        }
+
+        [TestMethod]
+        public void Test_Equal()
+        {
+            var descriptor = new SortDescriptor("Foo", SortOrder.Descending);
+            var other = new SortDescriptor("Foo", SortOrder.Descending);
+            Assert.IsTrue(descriptor.Equals(other));
+            Assert.AreEqual(descriptor.GetHashCode(), other.GetHashCode());
+        }
+
+        [TestMethod]
+        public void Test_ToString()
+        {
+            var descriptor = new SortDescriptor();
+            Assert.AreEqual("", descriptor.ToString());
+            descriptor = new SortDescriptor("Foo", SortOrder.None);
+            Assert.AreEqual("Foo", descriptor.ToString());
+            descriptor = new SortDescriptor("Foo", SortOrder.Ascending);
+            Assert.AreEqual("Foo ASC", descriptor.ToString());
+            descriptor = new SortDescriptor("Foo", SortOrder.Descending);
+            Assert.AreEqual("Foo DESC", descriptor.ToString());
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void Test_Create_When_Property_Not_Exist()
+        {
+            SortDescriptor.Create(typeof(SortDescriptor), "Foo", SortOrder.Descending);
+        }
+
+        [TestMethod]
+        public void Test_Create()
+        {
+            var expected = new SortDescriptor("PropertyName", SortOrder.Descending);
+            var actual = SortDescriptor.Create(typeof(SortDescriptor), "PropertyName", SortOrder.Descending);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ Masasamjant.Framework/Linq/SortDescriptor.cs
@@ -0,0 +1,131 @@
+﻿using System.Reflection;
+using System.Text.Json.Serialization;
+
+namespace Masasamjant.Linq
+{
+    /// <summary>
+    /// Describes sorting by specified property name.
+    /// </summary>
+    public sealed class SortDescriptor : IEquatable<SortDescriptor>
+    {
+        /// <summary>
+        /// Initializes new instance of the <see cref="SortDescriptor"/> class.
+        /// </summary>
+        /// <param name="propertyName">The property name.</param>
+        /// <param name="sortOrder">The sort order.</param>
+        /// <exception cref="ArgumentException">If value of <paramref name="sortOrder"/> is not defined.</exception>
+        public SortDescriptor(string propertyName, SortOrder sortOrder)
+        {
+            if (!Enum.IsDefined(sortOrder))
+                throw new ArgumentException("The value is not defined.", nameof(sortOrder));
+
+            PropertyName = propertyName;
+            SortOrder = sortOrder;
+        }
+
+        /// <summary>
+        /// Initializes new default instance of the <see cref="SortDescriptor"/> class.
+        /// </summary>
+        public SortDescriptor()
+        { }
+
+        /// <summary>
+        /// Gets the property name to sort by. Default is empty string.
+        /// </summary>
+        [JsonInclude]
+        public string PropertyName { get; internal set; } = string.Empty;
+
+        /// <summary>
+        /// Gets the sort order. Default is <see cref="SortOrder.None"/>.
+        /// </summary>
+        [JsonInclude]
+        public SortOrder SortOrder { get; internal set; } = SortOrder.None;
+
+        /// <summary>
+        /// Check if other <see cref="SortDescriptor"/> is equal to this instance.
+        /// </summary>
+        /// <param name="other">The other <see cref="SortDescriptor"/>.</param>
+        /// <returns><c>true</c> if <paramref name="other"/> is equal to this instance; <c>false</c> otherwise.</returns>
+        public bool Equals(SortDescriptor? other)
+        {
+            return other != null &&
+                PropertyName.Equals(other.PropertyName, StringComparison.Ordinal) &&
+                SortOrder == other.SortOrder;
+        }
+
+        /// <summary>
+        /// Check if object instance is <see cref="SortDescriptor"/> and equal to this instance.
+        /// </summary>
+        /// <param name="obj">The object instance.</param>
+        /// <returns><c>true</c> if <paramref name="obj"/> is <see cref="SortDescriptor"/> and equal to this instance; <c>false</c> otherwise.</returns>
+        public override bool Equals(object? obj)
+        {
+            return Equals(obj as SortDescriptor);
+        }
+
+        /// <summary>
+        /// Gets hash code for this instance.
+        /// </summary>
+        /// <returns>A hash code.</returns>
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(PropertyName, SortOrder);
+        }
+
+        /// <summary>
+        /// Gets the string presentation.
+        /// </summary>
+        /// <returns>A string presentation.</returns>
+        public override string ToString()
+        {
+            if (PropertyName.Length == 0)
+                return string.Empty;
+
+            var orderBy = SortOrder.ToSql();
+
+            if (orderBy.Length > 0)
+                return PropertyName + " " + orderBy;
+
+            return PropertyName;
+        }
+
+        /// <summary>
+        /// Creates <see cref="SortDescriptor"/> for the specified property of specified object instance 
+        /// ensuring that object has public instance get property with specified name.
+        /// </summary>
+        /// <param name="obj">The object instance.</param>
+        /// <param name="propertyName">The property name.</param>
+        /// <param name="sortOrder">The sort order.</param>
+        /// <returns>A <see cref="SortDescriptor"/>.</returns>
+        /// <exception cref="ArgumentException">
+        /// If type of <paramref name="obj"/> does not have public instance property specified by <paramref name="propertyName"/>.
+        /// -or-
+        /// If value of <paramref name="sortOrder"/> is not defined.
+        /// </exception>
+        public static SortDescriptor Create(object obj, string propertyName, SortOrder sortOrder)
+        {
+            return Create(obj.GetType(), propertyName, sortOrder);
+        }
+
+        /// <summary>
+        /// Creates <see cref="SortDescriptor"/> for the specified property of specified type ensuring 
+        /// that type has public instance get property with specified name.
+        /// </summary>
+        /// <param name="type">The type.</param>
+        /// <param name="propertyName">The property name.</param>
+        /// <param name="sortOrder">The sort order.</param>
+        /// <returns>A <see cref="SortDescriptor"/>.</returns>
+        /// <exception cref="ArgumentException">
+        /// If <paramref name="type"/> does not have public instance property specified by <paramref name="propertyName"/>.
+        /// -or-
+        /// If value of <paramref name="sortOrder"/> is not defined.
+        /// </exception>
+        public static SortDescriptor Create(Type type, string propertyName, SortOrder sortOrder)
+        {
+            var property = type.GetProperty(propertyName, BindingFlags.Public | BindingFlags.Instance | BindingFlags.GetProperty);
+            if (property == null)
+                throw new ArgumentException($"The type '{type}' does not have public instance get property of '{propertyName}'.", nameof(propertyName));
+            return new SortDescriptor(propertyName, sortOrder);
+        }
+    }
+}
