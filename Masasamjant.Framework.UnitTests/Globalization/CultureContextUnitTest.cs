0cef56c0144f81cbe012b2bd0a6996cee1268385
Matti Jantti <m_jantti@hotmail.com>
2025-03-16 09:02:17 +0200

    Add culture context

---
+++ Masasamjant.Framework.UnitTests/Globalization/CultureContextUnitTest.cs
@@ -0,0 +1,55 @@
+﻿using System.Globalization;
+
+namespace Masasamjant.Globalization
+{
+    [TestClass]
+    public class CultureContextUnitTest : UnitTest
+    {
+        [TestMethod]
+        public void Test_CultureContext()
+        {
+            CultureInfo c = CultureInfo.CurrentCulture;
+            CultureInfo u = CultureInfo.CurrentUICulture;
+
+            try
+            {
+                CultureInfo enUS = new CultureInfo("en-US");
+                CultureInfo.CurrentCulture = enUS;
+                CultureInfo.CurrentUICulture = enUS;
+                CultureInfo fi = new CultureInfo("fi-FI");
+
+                using (var context = new CultureContext(fi))
+                {
+                    Assert.IsTrue(CultureInfo.CurrentCulture.Equals(fi));
+                    Assert.IsTrue(CultureInfo.CurrentUICulture.Equals(fi));
+                }
+
+                Assert.IsTrue(CultureInfo.CurrentCulture.Equals(enUS));
+                Assert.IsTrue(CultureInfo.CurrentUICulture.Equals(enUS));
+
+                using (var context = new CultureContext(fi, null))
+                {
+                    Assert.IsTrue(CultureInfo.CurrentCulture.Equals(fi));
+                    Assert.IsTrue(CultureInfo.CurrentUICulture.Equals(enUS));
+                }
+
+                Assert.IsTrue(CultureInfo.CurrentCulture.Equals(enUS));
+                Assert.IsTrue(CultureInfo.CurrentUICulture.Equals(enUS));
+
+                using (var context = new CultureContext(null, fi))
+                {
+                    Assert.IsTrue(CultureInfo.CurrentCulture.Equals(enUS));
+                    Assert.IsTrue(CultureInfo.CurrentUICulture.Equals(fi));
+                }
+
+                Assert.IsTrue(CultureInfo.CurrentCulture.Equals(enUS));
+                Assert.IsTrue(CultureInfo.CurrentUICulture.Equals(enUS));
+            }
+            finally
+            {
+                CultureInfo.CurrentCulture = c;
+                CultureInfo.CurrentUICulture = u;
+            }
+        }
+    }
+}
---
+++ Masasamjant.Framework/Globalization/CultureContext.cs
@@ -0,0 +1,75 @@
+﻿using System.Globalization;
+
+namespace Masasamjant.Globalization
+{
+    /// <summary>
+    /// Represents context where specified cultures replace current cultures. When instance is creates, then specified 
+    /// cultures will be used instead of current ones. When context is disposed, original cultures are restored.
+    /// </summary>
+    public sealed class CultureContext : IDisposable
+    {
+        private readonly CultureInfo? restoreCulture;
+        private readonly CultureInfo? restoreUICulture;
+        private bool disposed;
+
+        /// <summary>
+        /// Initializes new instance of the <see cref="CultureContext"/> class with specified culture to use as <see cref="CultureInfo.CurrentCulture"/>
+        /// and <see cref="CultureInfo.CurrentUICulture"/> while context is in use.
+        /// </summary>
+        /// <param name="culture">The culture to use as <see cref="CultureInfo.CurrentCulture"/> and <see cref="CultureInfo.CurrentUICulture"/> or <c>null</c>, if cultures not changed.</param>
+        public CultureContext(CultureInfo? culture)
+            : this(culture, culture)
+        { }
+
+        /// <summary>
+        /// Initializes new instance of the <see cref="CultureContext"/> class with specified cultures to use while context
+        /// is in use.
+        /// </summary>
+        /// <param name="currentCulture">The current culture to use or <c>null</c>, if current culture not changed.</param>
+        /// <param name="currentUICulture">The current UI culture to use or <c>null</c>, if current culture not changed.</param>
+        public CultureContext(CultureInfo? currentCulture, CultureInfo? currentUICulture)
+        {
+            if (currentCulture != null)
+            {
+                restoreCulture = CultureInfo.CurrentCulture;
+                CultureInfo.CurrentCulture = currentCulture;
+            }
+
+            if (currentUICulture != null)
+            {
+                restoreUICulture = CultureInfo.CurrentUICulture;
+                CultureInfo.CurrentUICulture = currentUICulture;
+            }
+        }
+
+        /// <summary>
+        /// Finalizes current instance and restores original cultures.
+        /// </summary>
+        ~CultureContext()
+        {
+            RestoreCultures();
+        }
+
+        /// <summary>
+        /// Disposes current instance and restores original cultures.
+        /// </summary>
+        /// <exception cref="ObjectDisposedException">If instance is already disposed.</exception>
+        public void Dispose()
+        {
+            if (disposed)
+                throw new ObjectDisposedException(GetType().FullName);
+            disposed = true;
+            RestoreCultures();
+            GC.SuppressFinalize(this);
+        }
+
+        private void RestoreCultures()
+        {
+            if (restoreCulture != null)
+                CultureInfo.CurrentCulture = restoreCulture;
+
+            if (restoreUICulture != null)
+                CultureInfo.CurrentUICulture = restoreUICulture;
+        }
+    }
+}
