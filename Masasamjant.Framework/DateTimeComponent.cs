e8781b6ceac29c631f9cd6b9afd227cf4b9e0994
Matti Jantti <m_jantti@hotmail.com>
2025-03-16 09:15:00 +0200

    Add time and date and time components

---
+++ Masasamjant.Framework/DateTimeComponent.cs
@@ -0,0 +1,65 @@
+﻿using Masasamjant.Resources;
+using Masasamjant.Resources.Strings;
+
+namespace Masasamjant
+{
+    /// <summary>
+    /// Defines components of date and time.
+    /// </summary>
+    public enum DateTimeComponent : int
+    {
+        /// <summary>
+        /// Ticks
+        /// </summary>
+        [ResourceString(nameof(DateTimeComponentResource.Ticks), typeof(DateTimeComponentResource), UseNonPublicResource = true)]
+        Ticks = 0,
+
+        /// <summary>
+        /// Microsecond
+        /// </summary>
+        [ResourceString(nameof(DateTimeComponentResource.Microsecond), typeof(DateTimeComponentResource), UseNonPublicResource = true)]
+        Microsecond = 1,
+
+        /// <summary>
+        /// Millisecond
+        /// </summary>
+        [ResourceString(nameof(DateTimeComponentResource.Millisecond), typeof(DateTimeComponentResource), UseNonPublicResource = true)]
+        Millisecond = 2,
+
+        /// <summary>
+        /// Second
+        /// </summary>
+        [ResourceString(nameof(DateTimeComponentResource.Second), typeof(DateTimeComponentResource), UseNonPublicResource = true)]
+        Second = 3,
+
+        /// <summary>
+        /// Minute
+        /// </summary>
+        [ResourceString(nameof(DateTimeComponentResource.Minute), typeof(DateTimeComponentResource), UseNonPublicResource = true)]
+        Minute = 4,
+
+        /// <summary>
+        /// Hour
+        /// </summary>
+        [ResourceString(nameof(DateTimeComponentResource.Hour), typeof(DateTimeComponentResource), UseNonPublicResource = true)]
+        Hour = 5,
+
+        /// <summary>
+        /// Day
+        /// </summary>
+        [ResourceString(nameof(DateTimeComponentResource.Day), typeof(DateTimeComponentResource), UseNonPublicResource = true)]
+        Day = 6,
+
+        /// <summary>
+        /// Month
+        /// </summary>
+        [ResourceString(nameof(DateTimeComponentResource.Month), typeof(DateTimeComponentResource), UseNonPublicResource = true)]
+        Month = 7,
+
+        /// <summary>
+        /// Year
+        /// </summary>
+        [ResourceString(nameof(DateTimeComponentResource.Year), typeof(DateTimeComponentResource), UseNonPublicResource = true)]
+        Year = 8
+    }
+}
---
+++ Masasamjant.Framework/TimeComponent.cs
@@ -0,0 +1,41 @@
+﻿using Masasamjant.Resources;
+using Masasamjant.Resources.Strings;
+
+namespace Masasamjant
+{
+    /// <summary>
+    /// Defines components of time.
+    /// </summary>
+    public enum TimeComponent : int
+    {
+        /// <summary>
+        /// Hour component.
+        /// </summary>
+        [ResourceString(nameof(TimeComponentResource.Hour), typeof(TimeComponentResource), UseNonPublicResource = true)]
+        Hour = 0,
+
+        /// <summary>
+        /// Minute component.
+        /// </summary>
+        [ResourceString(nameof(TimeComponentResource.Minute), typeof(TimeComponentResource), UseNonPublicResource = true)]
+        Minute = 1,
+
+        /// <summary>
+        /// Second component.
+        /// </summary>
+        [ResourceString(nameof(TimeComponentResource.Second), typeof(TimeComponentResource), UseNonPublicResource = true)]
+        Second = 2,
+
+        /// <summary>
+        /// Millisecond component.
+        /// </summary>
+        [ResourceString(nameof(TimeComponentResource.Millisecond), typeof(TimeComponentResource), UseNonPublicResource = true)]
+        Millisecond = 3,
+
+        /// <summary>
+        /// Microsecond component.
+        /// </summary>
+        [ResourceString(nameof(TimeComponentResource.Microsecond), typeof(TimeComponentResource), UseNonPublicResource = true)]
+        Microsecond = 4
+    }
+}
