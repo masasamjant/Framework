3f6e5268f39d957d4507c9fc7e89b7d87b51c1c7
Matti Jantti <m_jantti@hotmail.com>
2025-04-28 08:15:18 +0300

    Add double helper

---
+++ Masasamjant.Framework.UnitTests/DoubleHelperUnitTest.cs
@@ -0,0 +1,30 @@
+﻿namespace Masasamjant
+{
+    [TestClass]
+    public class DoubleHelperUnitTest : UnitTest
+    {
+        [TestMethod]
+        public void Test_Constants()
+        { 
+            Assert.AreEqual(0.0, DoubleHelper.Zero);
+        }
+
+        [TestMethod]
+        public void Test_ReplaceNaN()
+        {
+            Assert.AreEqual(0.0, DoubleHelper.ReplaceNaN(double.NaN, 0.0));
+            Assert.AreEqual(1.0, DoubleHelper.ReplaceNaN(1.0, 0.0));
+            Assert.ThrowsException<ArgumentException>(() => DoubleHelper.ReplaceNaN(1.0, double.NaN));
+        }
+
+        [TestMethod]
+        public void Test_ReplaceInfinity()
+        {
+            Assert.AreEqual(0.0, DoubleHelper.ReplaceInfinity(double.PositiveInfinity, 0.0));
+            Assert.AreEqual(0.0, DoubleHelper.ReplaceInfinity(double.NegativeInfinity, 0.0));
+            Assert.AreEqual(1.0, DoubleHelper.ReplaceInfinity(1.0, 0.0));
+            Assert.ThrowsException<ArgumentException>(() => DoubleHelper.ReplaceInfinity(1.0, double.PositiveInfinity));
+            Assert.ThrowsException<ArgumentException>(() => DoubleHelper.ReplaceInfinity(1.0, double.NegativeInfinity));
+        }
+    }
+}
---
+++ Masasamjant.Framework/DoubleHelper.cs
@@ -0,0 +1,43 @@
+﻿namespace Masasamjant
+{
+    /// <summary>
+    /// Provides helper methods to work with <see cref="double"/> values.
+    /// </summary>
+    public static class DoubleHelper
+    {
+        /// <summary>
+        /// Value of zero.
+        /// </summary>
+        public const double Zero = default;
+
+        /// <summary>
+        /// Replace not-a-number with specified value.
+        /// </summary>
+        /// <param name="value">The initial value.</param>
+        /// <param name="replacement">The replacement value.</param>
+        /// <returns>A <paramref name="value"/> if not <see cref="double.NaN"/>; otherwise <paramref name="replacement"/>.</returns>
+        /// <exception cref="ArgumentException">If <paramref name="replacement"/> is <see cref="double.NaN"/>.</exception>
+        public static double ReplaceNaN(this double value, double replacement)
+        {
+            if (double.IsNaN(replacement))
+                throw new ArgumentException("Replacement value can not be double.NaN.", nameof(replacement));
+
+            return double.IsNaN(value) ? replacement : value;
+        }
+
+        /// <summary>
+        /// Replace positive or negative infinity with specified value.
+        /// </summary>
+        /// <param name="value">The initial value.</param>
+        /// <param name="replacement">The replacement value.</param>
+        /// <returns>A <paramref name="value"/> if not positive or negative infinity; otherwise <paramref name="replacement"/>.</returns>
+        /// <exception cref="ArgumentException">If <paramref name="replacement"/> is positive or negative infinity.</exception>
+        public static double ReplaceInfinity(this double value, double replacement)
+        {
+            if (double.IsInfinity(replacement))
+                throw new ArgumentException("Replacement value cannot be positive or negative infinity.", nameof(replacement));
+
+            return double.IsInfinity(value) ? replacement : value;
+        }
+    }
+}
