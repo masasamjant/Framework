8d5bce3ef15a86581c9973b281c22b53bf48f62e
Matti Jantti <m_jantti@hotmail.com>
2025-08-24 18:59:42 +0300

    Add navigation item interface

---
+++ Masasamjant.Framework.Web/ViewModels/ISupportHtmlAttributes.cs
@@ -0,0 +1,13 @@
+﻿namespace Masasamjant.Web.ViewModels
+{
+    /// <summary>
+    /// Represents view model that supports HTML attributes dictionary.
+    /// </summary>
+    public interface ISupportHtmlAttributes
+    {
+        /// <summary>
+        /// Gets or sets HTML attributes.
+        /// </summary>
+        IDictionary<string, object?> HtmlAttributes { get; }
+    }
+}
---
+++ Masasamjant.Framework.Web/ViewModels/Navigation/INavigationItem.cs
@@ -0,0 +1,28 @@
+﻿namespace Masasamjant.Web.ViewModels.Navigation
+{
+    /// <summary>
+    /// Represents navigation item.
+    /// </summary>
+    public interface INavigationItem : ISupportCssClass, ISupportDisabledCssClass, ISupportEnabledState, ISupportHtmlAttributes
+    {
+        /// <summary>
+        /// Gets or sets the item text.
+        /// </summary>
+        string Text { get; set; }
+
+        /// <summary>
+        /// Gets or sets the URL to navigatate.
+        /// </summary>
+        string Url { get; set; }
+
+        /// <summary>
+        /// Gets route aka query string parameters.
+        /// </summary>
+        IDictionary<string, object?> RouteParameters { get; }
+
+        /// <summary>
+        /// Gets child navigation items.
+        /// </summary>
+        IEnumerable<INavigationItem> Children { get; }
+    }
+}
