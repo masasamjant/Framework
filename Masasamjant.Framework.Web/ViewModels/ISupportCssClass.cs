c2af763b4ab69eb5725d702b7b52bc59c9fe6754
Matti Jantti <m_jantti@hotmail.com>
2025-08-24 17:55:53 +0300

    Add web framework

---
+++ Masasamjant.Framework.Web/ViewModels/ISupportCssClass.cs
@@ -0,0 +1,13 @@
+﻿namespace Masasamjant.Web.ViewModels
+{
+    /// <summary>
+    /// Represents view model that support CSS class.
+    /// </summary>
+    public interface ISupportCssClass
+    {
+        /// <summary>
+        /// Gets or sets name(s) of CSS classes applied to element.
+        /// </summary>
+        string CssClass { get; set; }
+    }
+}
---
+++ Masasamjant.Framework.Web/ViewModels/ISupportDisabledCssClass.cs
@@ -0,0 +1,13 @@
+﻿namespace Masasamjant.Web.ViewModels
+{
+    /// <summary>
+    /// Represents view model that support disabled CSS class.
+    /// </summary>
+    public interface ISupportDisabledCssClass
+    {
+        /// <summary>
+        /// Gets or sets name(s) of CSS classes applied to element when element is disabled.
+        /// </summary>
+        string DisabledCssClass { get; }
+    }
+}
