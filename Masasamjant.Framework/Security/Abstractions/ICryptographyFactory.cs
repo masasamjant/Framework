ae89d921c617916c92ddf671a245d4a37c3eb187
Matti Jantti <m_jantti@hotmail.com>
2025-12-11 22:38:03 +0200

    Add cryptography factory interface and implementation for aes

---
+++ Masasamjant.Framework/Security/Abstractions/ICryptographyFactory.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Masasamjant.Security.Abstractions
+{
+    public interface ICryptographyFactory
+    {
+        IDataCryptography CreateDataCryptography();
+
+        IFileCryptography CreateFileCryptography();
+
+        IStreamCryptography CreateStreamCryptography();
+    }
+
+    public interface ICryptographyFactory<TCryptoKey> where TCryptoKey : CryptoKey
+    {
+        IDataCryptography<TCryptoKey> CreateDataCryptography();
+
+        IFileCryptography<TCryptoKey> CreateFileCryptography();
+
+        IStreamCryptography<TCryptoKey> CreateStreamCryptography();
+
+        CryptoKeyExport<TCryptoKey> CreateCryptoKeyExport();
+
+        CryptoKeyImport<TCryptoKey> CreateCryptoKeyImport();
+    }
+}
---
+++ Masasamjant.Framework/Security/AesCryptographyFactory.cs
@@ -0,0 +1,60 @@
+﻿using Masasamjant.Security.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Masasamjant.Security
+{
+    public sealed class AesCryptographyFactory : ICryptographyFactory<AesCryptoKey>, ICryptographyFactory
+    {
+        private readonly HashAlgorithmName? hashAlgorithmName;
+        private readonly int? iterations;
+
+        public AesCryptographyFactory(HashAlgorithmName? hashAlgorithmName = null, int? iterations = null)
+        {
+            this.hashAlgorithmName = hashAlgorithmName;
+            this.iterations = iterations;
+        }
+
+        public CryptoKeyExport<AesCryptoKey> CreateCryptoKeyExport()
+        {
+            return new AesCryptoKeyExport();
+        }
+
+        public CryptoKeyImport<AesCryptoKey> CreateCryptoKeyImport()
+        {
+            return new AesCryptoKeyImport();
+        }
+
+        public IDataCryptography<AesCryptoKey> CreateDataCryptography()
+        {
+            return new AesDataCryptography(hashAlgorithmName, iterations);
+        }
+
+        public IFileCryptography<AesCryptoKey> CreateFileCryptography()
+        {
+            return new AesFileCryptography(hashAlgorithmName, iterations);
+        }
+
+        public IStreamCryptography<AesCryptoKey> CreateStreamCryptography()
+        {
+            return new AesStreamCryptography(hashAlgorithmName, iterations);
+        }
+
+        IDataCryptography ICryptographyFactory.CreateDataCryptography()
+        {
+            return CreateDataCryptography();
+        }
+
+        IFileCryptography ICryptographyFactory.CreateFileCryptography()
+        {
+            return CreateFileCryptography();
+        }
+
+        IStreamCryptography ICryptographyFactory.CreateStreamCryptography()
+        {
+            return CreateStreamCryptography();
+        }
+    }
+}
